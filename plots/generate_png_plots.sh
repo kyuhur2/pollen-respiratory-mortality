fb7ce64ab6dc53cd6a5181590e245b449c597e37
Kyu Hur <kyu.hur@mapbox.com>
2024-09-24 04:01:19 +0900

    generate .png plots; move magick description in README into an executable shell script

---
+++ plots/generate_png_plots.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+magick figure1.pdf figure1.png
+magick figure2.pdf figure2.png
+magick figure3.pdf figure3.png
+magick figureS1.pdf figureS1.png
+magick figureS2.pdf figureS2.png
+magick figureS3.pdf figureS3.png
+magick figureS4.pdf figureS4.png
+magick figureS5.pdf figureS5.png
